eafcf8e3028f89007606590d402a6fa72ce010ca
NodyHub <jan.harrie+github@gmail.com>
2017-07-02 15:25:47 +0200

    added cfg handling

--- flathunter/sender_telegram.py
+++ flathunter/sender_telegram.py
@@ -9,17 +9,21 @@ class SenderTelegram(Base):
 
     def __init__(self, cfg):
         self.cfg = cfg
+        self.bot_token = self.cfg.get('telegram', dict()).get('bot_token', '')
+        self.receiver_ids = self.cfg.get('telegram', dict()).get('receiver_ids', list())
 
-    def send_msg(self, bot_token, chat_id, message):
-        url = 'https://api.telegram.org/%s/sendMessage?chat_id=%i&text=%s'
-        text = urllib.parse.quote_plus(message.encode('utf-8'))
-        qry = url % (bot_token, chat_id, text)
-        self.__log__.debug("Retrieving URL %s" % qry)
-        resp = requests.get(qry)
-        self.__log__.debug("Got response (%i): %s" % (resp.status_code, resp.content))
-        data = resp.json()
 
-        # handle error
-        if resp.status_code != 200:
-            sc = resp.status_code
-            self.__log__.error("When sending bot message, we got status %i with message: %s" % (sc, data))
+    def send_msg(self, message):
+        for chat_id in self.receiver_ids:
+            url = 'https://api.telegram.org/bot%s/sendMessage?chat_id=%i&text=%s'
+            text = urllib.parse.quote_plus(message.encode('utf-8'))
+            qry = url % (self.bot_token, chat_id, text)
+            self.__log__.debug("Retrieving URL %s" % qry)
+            resp = requests.get(qry)
+            self.__log__.debug("Got response (%i): %s" % (resp.status_code, resp.content))
+            data = resp.json()
+
+            # handle error
+            if resp.status_code != 200:
+                sc = resp.status_code
+                self.__log__.error("When sending bot message, we got status %i with message: %s" % (sc, data))
