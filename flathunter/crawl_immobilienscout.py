e892018b2fa3ff9be72b8af6c2b26f8ca5fc9064
Zaunei <jan.schnurpfeil@pi-lar.net>
2018-04-10 21:47:34 +0200

    Quickfix Immobillienscout

--- flathunter/crawl_immobilienscout.py
+++ flathunter/crawl_immobilienscout.py
@@ -53,18 +53,21 @@ class CrawlImmobilienscout:
         address_fields = soup.find_all(lambda e: e.has_attr('class') and 'result-list-entry__address' in e['class'])
 
         for idx, title_el in enumerate(title_elements):
-            attr_els = attr_container_els[idx].find_all('dd')
-            address = address_fields[idx].text.strip().replace(',', '')
-            details = {
-                'id': expose_ids[idx],
-                'url': expose_urls[idx],
-                'title': title_el.text.strip().replace('NEU', ''),
-                'price': attr_els[0].text.strip().split(' ')[0].strip(),
-                'size': attr_els[1].text.strip().split(' ')[0].strip() + " qm",
-                'rooms': attr_els[2].text.strip().split(' ')[0].strip() + " Zi.",
-                'address': address
-            }
-            entries.append(details)
+            try:
+                attr_els = attr_container_els[idx].find_all('dd')
+                address = address_fields[idx].text.strip().replace(',', '')
+                details = {
+                    'id': expose_ids[idx],
+                    'url': expose_urls[idx],
+                    'title': title_el.text.strip().replace('NEU', ''),
+                    'price': attr_els[0].text.strip().split(' ')[0].strip(),
+                    'size': attr_els[1].text.strip().split(' ')[0].strip() + " qm",
+                    'rooms': attr_els[2].text.strip().split(' ')[0].strip() + " Zi.",
+                    'address': address
+                }
+                entries.append(details)
+            except:
+                print("Error")
 
         self.__log__.debug('extracted: ' + str(entries))
         return entries
